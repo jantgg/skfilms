15109441c75193eb9915bcd524e8b71f83aff148
unknown <smosh360@hotmail.com>
2023-12-06 16:02:44 +0100

    hola

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,13 @@ export default function RootLayout({ children }) {
           name="keywords"
           content="SK FILMS, Videografía, Fotografía, Gijón, Marbella, Carlos Villaverde"
         />
+        <meta property="og:title" content={metadata.title} />
+<meta property="og:description" content={metadata.description} />
+<meta property="og:url" content="https://skfilms.es" />
+<meta property="og:type" content="website" />
+<meta property="og:image" content="https://res.cloudinary.com/dsbp8pqdb/image/upload/v1701874819/fotopaginashuck_mkrtb2.png" alt="" />
               <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet"></link>
  
       </Head>
